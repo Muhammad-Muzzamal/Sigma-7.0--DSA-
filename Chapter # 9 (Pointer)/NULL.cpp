e5031292f481fe61566ebc872df66f6c6f553b69
Muzzamal <ashrafmuzzamal7@gmail.com>
2025-04-05 17:26:36 +0500

    Null pointer

--- Chapter # 9 (Pointer)/NULL.cpp
+++ Chapter # 9 (Pointer)/NULL.cpp
@@ -3,6 +3,25 @@ using namespace std;
 int main()
 {
     int *ptr = NULL;
-    cout << *ptr;
+    cout << *ptr; //Empty
+    // cout << ptr ; //Empty
     return 0;
 }
+
+
+/*
+============>  NULL POINTER <================
+    we assigned the NULL value to a pointer to show taht it 
+    does not point any location 
+
+Example :
+
+    int *ptr = NULL;
+    cout << *ptr << endl; ->(it does not show any value because it does point to any location)
+    cout << ptr << endl;  ->(it does not show any value because it does point to any location)
+
+
+    we can not dereferance null pointer because it does not have any address
+    it shows an error called " Sigment Error"
+
+*/
