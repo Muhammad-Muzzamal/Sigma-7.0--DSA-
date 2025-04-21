06652b760c7bd29d66fa2d17ef07952ae2f3b382
Muhammad Muzzamal <ashrafmuzzamal7@gmail.com>
2025-04-21 10:32:23 -0700

    find the left max and right max of bars to calculate the water trapped

---
+++ Chapter # 10 (Arrays)/TrappedWater/trappedWater.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int trappedWater(int *height, int size) {
+    int leftmax[20000];
+    leftmax[0] = height[0];
+    cout << leftmax[0] << ", ";
+
+    for (int i = 1; i < size; i++)
+    {
+        leftmax[i] = max(leftmax[i-1], height[i-1]);
+        cout << leftmax[i] << ", ";
+    }
+    cout << endl;
+
+    int rightMax[20000];
+    rightMax[size-1] = height[size-1];
+    for (int i = size-2; i >= 0; i--)
+    {
+        rightMax[i] = max(rightMax[i+1], height[i+1]);
+        cout << rightMax[i] << ", ";
+    }
+    int waterTrapped = 0;
+    for (int i = 0; i < size; i++)
+    {
+        /* code */
+    }
+    
+    
+    
+}
+int main () {
+    int height[] = {4, 2, 0, 6, 3, 2, 5};
+    int size = sizeof(height) / sizeof(int);
+    trappedWater(height, size);
+    return 0;
+}
