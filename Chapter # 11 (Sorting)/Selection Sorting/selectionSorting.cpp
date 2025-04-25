56961df1b6e6146c30129233db5b054e0d986e91
Muhammad Muzzamal <ashrafmuzzamal7@gmail.com>
2025-04-25 09:23:08 -0700

    add display function

--- Chapter # 11 (Sorting)/Selection Sorting/selectionSorting.cpp
+++ Chapter # 11 (Sorting)/Selection Sorting/selectionSorting.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
+void display(int *arr, int n) {
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << ", ";
+    }
+    cout << endl;
+}
 int main () {
     return 0;
 }
