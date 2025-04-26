2b46d1ac29b6f30dd1f6a318217e5620aed5cdb6
Muhammad-Muzzamal <ashrafmuzzamal7@gmail.com>
2025-04-26 09:26:16 -0700

    add counting sort fungtion to my assignment

--- Chapter # 11 (Sorting)/Assignment/assignment.cpp
+++ Chapter # 11 (Sorting)/Assignment/assignment.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <climits>
 using namespace std;
 void display(int *arr, int n) {
     for(int i=0; i<n; i++) {
@@ -42,9 +43,31 @@ void insertionSort(int arr[], int n) {
     display(arr, n);
 
 }
+void countingSort(int arr[], int n) {
+    int freq[100000];
+    int minVal = INT_MAX;
+    int maxVal = INT_MIN;
+    for(int i=0; i<n; i++) {
+        minVal = min(minVal, arr[i]);
+        maxVal = max(maxVal, arr[i]);
+    }
+
+    for(int i=0; i<n; i++) {
+        freq[arr[i]]++;
+    }
+    for(int i=minVal, j=0; i<=maxVal; i++) {
+        while (freq[i] > 0) 
+        {
+           arr[j++] = i;
+           freq[i]--;
+        }
+        
+    }
+    display(arr, n);
+}
 int main () {
     int arr[] = {3,6,2,1,8,7,4,5,3,1};
     int n = sizeof(arr) / sizeof(int);
-    insertionSort(arr, n);
+    countingSort(arr, n);
     return 0;
 }
