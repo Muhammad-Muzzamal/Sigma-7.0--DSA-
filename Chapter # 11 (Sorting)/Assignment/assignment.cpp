cc67ea5011327474d12d00e84a900d1b474d9b7e
Muhammad-Muzzamal <ashrafmuzzamal7@gmail.com>
2025-04-26 09:10:49 -0700

    add inserion sort function to my assignment

--- Chapter # 11 (Sorting)/Assignment/assignment.cpp
+++ Chapter # 11 (Sorting)/Assignment/assignment.cpp
@@ -29,9 +29,22 @@ void selectionSort(int arr[], int n) {
     }
     display(arr, n);
 }
+void insertionSort(int arr[], int n) {
+    for(int i=1; i<n; i++) {
+        int curr = arr[i];
+        int prev = i-1;
+        while(prev >= 0 && arr[prev] > curr) {
+            swap(arr[prev], arr[prev+1]);
+            prev--;
+        }
+        arr[prev+1] = curr ;
+    }
+    display(arr, n);
+
+}
 int main () {
     int arr[] = {3,6,2,1,8,7,4,5,3,1};
     int n = sizeof(arr) / sizeof(int);
-    selectionSort(arr, n);
+    insertionSort(arr, n);
     return 0;
 }
