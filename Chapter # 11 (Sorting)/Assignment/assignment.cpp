1c341f01166698d039be4913a7a3c9dd404169a8
Muhammad-Muzzamal <ashrafmuzzamal7@gmail.com>
2025-04-26 09:02:09 -0700

    add selesctionSort function to the assingment

--- Chapter # 11 (Sorting)/Assignment/assignment.cpp
+++ Chapter # 11 (Sorting)/Assignment/assignment.cpp
@@ -17,9 +17,21 @@ void bubbleSort(int *arr, int n) {
     }
     display(arr, n); 
 }
+void selectionSort(int arr[], int n) {
+    for(int i=0; i<n-1; i++) {
+        int minIdx = i;
+        for(int j=i+1; j<n; j++) {
+            if(arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+        swap(arr[i], arr[minIdx]);
+    }
+    display(arr, n);
+}
 int main () {
     int arr[] = {3,6,2,1,8,7,4,5,3,1};
     int n = sizeof(arr) / sizeof(int);
-    bubbleSort(arr, n);
+    selectionSort(arr, n);
     return 0;
 }
