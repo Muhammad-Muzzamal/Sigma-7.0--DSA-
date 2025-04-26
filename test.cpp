62ffdb7ef816b8c4d352b3e21a6dfbc77a24257e
Muhammad-Muzzamal <ashrafmuzzamal7@gmail.com>
2025-04-26 09:30:39 -0700

    add counting sort function as a test

--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 // display function 
 void display(int *arr, int n) {
@@ -43,10 +44,32 @@ void insertionSort(int *arr, int n) {
     }
     display(arr, n);
 }
+//Counting sort
+void countingSort(int arr[], int n) {
+    int freq[100000];
+    int minVal = INT_MAX;
+    int maxVal = INT_MIN;
+    for(int i=0; i<n; i++) {
+        minVal = min(minVal, arr[i]);
+        maxVal = max(maxVal, arr[i]);
+    }
+
+    for(int i=0; i<n; i++) {
+        freq[arr[i]]++;
+    }
+    for(int i=minVal, j=0; i<=maxVal; i++) {
+        while (freq[i] > 0) 
+        {
+           arr[j++] = i;
+           freq[i]--;
+        }
+        
+    }
+    display(arr, n);
+}
 int main () {
-    int arr[] = {64, 25, 12, 22, 11};
+    int arr[] = {3,6,2,1,8,7,4,5,3,1};
     int n = sizeof(arr) / sizeof(int);
-    cout << "Bubble Sorting : ";
-    bubbleSort(arr, n);
+    
     return 0;
 }
