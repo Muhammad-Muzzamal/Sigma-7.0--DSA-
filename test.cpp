b4ee6d3b7e3266c3473a3925250a8af363c4328b
Muhammad-Muzzamal <ashrafmuzzamal7@gmail.com>
2025-04-27 11:26:56 -0700

    test

--- test.cpp
+++ test.cpp
@@ -110,7 +110,13 @@ void spiralMatrix(int mat[][4], int row, int col) {
     }
 }
 int main () {
-    
+    int n = 4;
+    int m = 4;
+    int matrix[4][4] = {{1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}};
+    spiralMatrix(matrix, n, m);
     
     return 0;
 }
