3564fda12c2b42b5cbab0d0b65d5ac586429611b
Muhammad-Muzzamal <ashrafmuzzamal7@gmail.com>
2025-04-27 11:24:30 -0700

    add spiral matrix function to test file

--- test.cpp
+++ test.cpp
@@ -75,11 +75,42 @@ void descSort(int arr[], int n) {
     sort(arr, arr+n, greater<int>());
     display(arr, n);
 }
+void spiralMatrix(int mat[][4], int row, int col) {
+    int srow = 0;
+    int scol = 0;
+    int erow = row - 1;
+    int ecol = col - 1;
+    while (erow >= srow && ecol >= scol) {
+        // top
+        for (int i = srow; i <= ecol; i++) {
+            cout << mat[srow][i] << ", ";
+        }
+        // right
+        for (int i = srow + 1; i <= erow; i++) {
+            cout << mat[i][ecol] << ", ";
+        }
+        // bottom
+        for (int i = ecol - 1; i >= scol; i--) {
+            if(srow == erow) {
+                break;
+            }
+            cout << mat[erow][i] << ", ";
+        }
+        // left
+        for (int i = erow - 1; i >= srow + 1; i--) {
+            if(scol == ecol) {
+                break;
+            }
+            cout << mat[i][scol] << " ";
+        }
+        srow++;
+        scol++;
+        erow--;
+        ecol--;
+    }
+}
 int main () {
-    int arr[] = {3,6,2,1,8,7,4,5,3,1};
-    int n = sizeof(arr) / sizeof(int);
-    descSort(arr, n);
-    ascSort(arr, n);
+    
     
     return 0;
 }
