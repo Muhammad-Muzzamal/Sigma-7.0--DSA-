67970b71c3b27a5e792de20edf86d660dfcd42d3
Muhammad-Muzzamal <ashrafmuzzamal7@gmail.com>
2025-05-06 11:48:43 -0700

    add assignment question

---
+++ Chapter # 13 (Char-&-Strings)/Assignment/q1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int countVowels(string str) {
+    int count = 0;
+    for(int i=0; i<str.length(); i++) {
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u') {
+            count++;
+        }
+    }
+    return count;
+}
+int main() {
+    string str;
+    cout << "Enter a string : ";
+    getline(cin, str);
+    cout << countVowels(str);
+    return 0;
+}
---
+++ Chapter # 13 (Char-&-Strings)/Assignment/q2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+bool closeString(string str1, string str2) {
+    if(str1 != str2) {
+        return false;
+    }
+    int count[26] = {0};
+    for(int i=0; i<str1.length(); i++) {
+        if(str1[i]-'a')
+
+    }
+}
+
+int main() {
+    string str = "hello";    
+    return 0;
+}
