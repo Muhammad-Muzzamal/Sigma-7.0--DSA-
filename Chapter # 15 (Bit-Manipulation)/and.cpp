9cb0aaf8951309c12e68d1c7f2150ef505553bc8
Muhammad-Muzzamal <ashrafmuzzamal7@gmail.com>
2025-05-09 10:01:48 -0700

    Bit manipulation

---
+++ Chapter # 15 (Bit-Manipulation)/and.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Bitwise AND operator : ";
+    cout << (3 & 5);
+    return 0;
+}
---
+++ Chapter # 15 (Bit-Manipulation)/or.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Bitwise OR operator : " << (3 | 5) << endl;
+    return 0;
+}
