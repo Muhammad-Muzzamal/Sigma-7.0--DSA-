1812abfdfa65653a72aca943108005fad12c2039
Muhammad-Muzzamal <ashrafmuzzamal7@gmail.com>
2026-03-04 19:10:14 +0500

    add code

--- DSA PROBLEMS/Stack/evalRPN.cpp
+++ DSA PROBLEMS/Stack/evalRPN.cpp
@@ -1,24 +1,14 @@
 #include <iostream>
 #include <vector>
-#include <string> // Required for stoi
-
 using namespace std;
-
 void evalRPN(vector<string> &tokens)
 {
     for (string s : tokens)
     {
-        // Check if the string is a number (and not an operator like "+" or "*")
-        if (isdigit(s[0]) || (s.size() > 1 && s[0] == '-')) {
-            int val = stoi(s); 
-            cout << val << " ";
-        } else {
-            cout << s << " "; // Print the operator as is
-        }
+        cout << int(s[0] - '0') << " ";
     }
     cout << endl;
 }
-
 int main()
 {
     vector<string> tokens = {"2","1","+","3","*"};
